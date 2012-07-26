90f6cb3aa840d4b8d302daa769b554ee0c64abfb
Anthony Doan <mythicalprogrammer@gmail.com>
2012-07-25 18:55:42 -0700

    added validate_base_sequence script

---
+++ chapters/chp_3/validate_base_sequence.py
@@ -0,0 +1,6 @@
+def validate_base_sequence(base_sequence, RNAflag = False):
+	"""Return True if the string base_sequence contains only upper- or \
+	T (or U, if RNAflag), C, A, and G characters, otherwise False"""
+	DNAbases = set('TCAGtcag')
+	RNAbases = set('UCAGucag')
+	return set(base_sequence).issubset(RNAbases if RNAflag else DNAbases)
